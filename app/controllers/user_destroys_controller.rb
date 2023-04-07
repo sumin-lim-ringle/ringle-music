36f66ecd73707f8b9449a99b16a6e8e844b1babe
Sumin Lim <sumin.lim@ringleplus.com>
2023-04-07 10:07:03 -0700

    ENG-1766 유저 삭제 API

---
+++ app/controllers/user_destroys_controller.rb
@@ -0,0 +1,22 @@
+class UserDestroysController < ApplicationController
+    skip_before_action :verify_authenticity_token
+    def destroy
+        # user table 에서 해당 유저 block
+        @user = User.find(params[:user_id])
+
+        # 개인 playlist 에 해당 아이디가 있다면 block 처리
+        @user_playlist =  Playlist.find_by(ownable_id: params[:user_id])
+        if @user_playlist
+            @user_playlist.update_attribute(:usable, 0)
+        end
+
+        # 해당 유저 block
+        @user.update_attribute(:usable, 0)
+
+        #user_group table 에서도 해당 유저 block
+        @user_group = UserGroup.where("user_id = ?", params[:user_id])
+        @user_group.each do | user |
+            user.update_attribute(:usable, 0)
+        end
+    end
+end
